fec03022ab2e9c6662ccbfced664b7d6c207b641
atli9908 <2579842828@qq.com>
2020-11-15 23:08:22 +0800

    完成商品分组

--- src/components/goods_group.vue
+++ src/components/goods_group.vue
@@ -6,54 +6,131 @@
                 <el-button plain size="small" class="zhed-btn"><i class="el-icon-caret-bottom"></i> 折叠全部</el-button>
                 <el-button plain size="small" style="float:right">新增一级分组</el-button>
             </div>
-            
             <div class="bg-purple jianju">
-                <el-table
-                    :show-header="false"
-                    :data="tableData5"
-                    style="width: 100%">
-                    <el-table-column type="expand">
-                        <template slot-scope="props">
-                            <el-form label-position="left" inline class="demo-table-expand">
-                            <el-form-item label="商品名称">
-                                <span>{{ props.row.name }}</span>
-                            </el-form-item>
-                            <el-form-item label="所属店铺">
-                                <span>{{ props.row.shop }}</span>
-                            </el-form-item>
-                            <el-form-item label="商品 ID">
-                                <span>{{ props.row.id }}</span>
-                            </el-form-item>
-                            <el-form-item label="店铺 ID">
-                                <span>{{ props.row.shopId }}</span>
-                            </el-form-item>
-                            <el-form-item label="商品分类">
-                                <span>{{ props.row.category }}</span>
-                            </el-form-item>
-                            <el-form-item label="店铺地址">
-                                <span>{{ props.row.address }}</span>
-                            </el-form-item>
-                            <el-form-item label="商品描述">
-                                <span>{{ props.row.desc }}</span>
-                            </el-form-item>
-                            </el-form>
-                        </template>
-                    </el-table-column>
-                    <el-table-column prop="id">
-                        <span style="color:#333">系统分组</span>
-                    </el-table-column>
-                    <el-table-column prop="desc">
-                        <div style="color:#333">
-                            <span style="margin-right:5px">分组显示 </span>
-                            <el-switch
+                <div class="thead" @click="showTbody">
+                    <el-row>
+                        <el-col :span="14">
+                            <div>
+                                <i :class="sicon"></i>
+                                <span>系统分组</span>
+                            </div>
+                        </el-col>
+                        <el-col :span="10">
+                            <div @click.stop="">
+                                <span>分组显示 </span>
+                                <el-switch
                                 v-model="value"
                                 active-color="#13ce66"
-                                inactive-color="#ff4949">
-                            </el-switch>
-                        </div> 
-                    </el-table-column>
-                </el-table>
+                                inactive-color="#ccc">
+                                </el-switch>
+                            </div>
+                        </el-col>
+                    </el-row>
+                </div>
+                <div class="tbody" v-if="isshow">
+                    <el-row>
+                        <el-col :span="13">
+                            <div>
+                                <a href="#">*新 品 上 架</a>
+                                <span> （系统自动读取最新上架的20个商品）</span>
+                            </div>
+                        </el-col>
+                        <el-col :span="10">
+                            <div>
+                                <span>不显示</span>
+                            </div>
+                        </el-col>
+                    </el-row>
+                    <el-row>
+                        <el-col :span="13">
+                            <div>
+                                <a href="#">*热 销 排 行</a>
+                                <span> （系统自动读取最新上架的20个商品）</span>
+                            </div>
+                        </el-col>
+                        <el-col :span="10">
+                            <div>
+                                <span>不显示</span>
+                            </div>
+                        </el-col>
+                    </el-row>
+                    <el-row>
+                        <el-col :span="13">
+                            <div>
+                                <a href="#">*列 表 中 隐 藏</a>
+                            </div>
+                        </el-col>
+                        <el-col :span="10">
+                            <div>
+                                <span>该分组默认不会展示在买家页面</span>
+                            </div>
+                        </el-col>
+                    </el-row>
+                </div>
             </div>
+            <div class="bg-purple jianju" v-for="item in primaryGroup" :key="item.title">
+                <div class="thead">
+                    <el-row>
+                        <el-col :span="14">
+                            <div>
+                                <i :class="sicon"></i>
+                                <span>{{item.title}}</span>
+                            </div>
+                        </el-col>
+                        <el-col :span="5">
+                            <div @click.stop="">
+                                <span>分组显示 </span>
+                                <el-switch
+                                v-model="item.status"
+                                @change="changeSwitch()"
+                                active-color="#13ce66"
+                                inactive-color="#ccc">
+                                </el-switch>
+                            </div>
+                        </el-col>
+                        <el-col :span="5">
+                            <div @click.stop="" class="caoz-a">
+                                <a href="#">新增二级分组</a>
+                                <a href="#">编辑</a>
+                                <a href="#">删除</a>
+                            </div>
+                        </el-col>
+                    </el-row>
+                </div>
+                <div class="tbody">
+                    <el-row v-for="subItem in item.subGroup">
+                        <el-col :span="7">
+                            <div class="imgTitle">
+                                <img :src="subItem.imgsrc" alt="">
+                                <span> {{subItem.subTitle}}</span>
+                            </div>
+                        </el-col>
+
+                        <el-col :span="6">
+                            <div>
+                                <span>商品数量：<a href="#">5</a> </span>
+                            </div>
+                        </el-col>
+                        
+                        <el-col :span="5">
+                            <div>
+                                <span>显示分组 </span>
+                                <el-switch
+                                v-model="subItem.status"
+                                active-color="#13ce66"
+                                inactive-color="#ccc">
+                                </el-switch>
+                            </div>
+                        </el-col>
+                        <el-col :span="6">
+                            <div class="caoz-a">
+                                <a href="#">编辑</a>
+                                <a href="#">删除</a>
+                            </div>
+                        </el-col>
+                    </el-row>
+                </div>
+            </div>           
         </div>
     </div>
 </template>
@@ -62,48 +139,90 @@
 export default {
     data(){
         return {
-            value: true,
-
-            tableData5: [{
-              id: '12987122',
-              name: '好滋好味鸡蛋仔',
-              category: '江浙小吃、小吃零食',
-              desc: '荷兰优质淡奶，奶香浓而不腻',
-              address: '上海市普陀区真北路',
-              shop: '王小虎夫妻店',
-              shopId: '10333'
-            }, {
-              id: '12987123',
-              name: '好滋好味鸡蛋仔',
-              category: '江浙小吃、小吃零食',
-              desc: '荷兰优质淡奶，奶香浓而66',
-              address: '上海市普陀区真北路',
-              shop: '王小虎夫妻店',
-              shopId: '10333'
-            }]
+            value:true,
+            isshow:true,
+            sicon:'el-icon-caret-right',
+            primaryGroup:[
+                {
+                    title:'洗家电器',
+                    status:true,
+                    subGroup:[
+                        {imgsrc:'/img/1.png',subTitle:'擦地机',status:true},
+                        {imgsrc:'/img/1.png',subTitle:'吸尘器',status:true},
+                        {imgsrc:'/img/1.png',subTitle:'蒸汽清洗机',status:false},
+                        {imgsrc:'/img/1.png',subTitle:'配件',status:false},
+                    ]
+                },
+                {
+                    title:'美容清洗',
+                    status:true,
+                    subGroup:[
+                        {imgsrc:'/img/1.png',subTitle:'高压清洗机',status:false},
+                        {imgsrc:'/img/1.png',subTitle:'吸尘器',status:false},
+                        {imgsrc:'/img/1.png',subTitle:'配件',status:false}
+                    ]
+                },
+            ]
         }
     },
-    methods: {
-        
-    }
+    methods:{   
+        showTbody(){    
+            this.isshow = !this.isshow
+            this.sicon = this.isshow ? 'el-icon-caret-bottom' : 'el-icon-caret-right'
+        },
+        changeSwitch(){
+            this.primaryGroup.forEach(primaryGroup =>{
+                if(primaryGroup.status){
+                    primaryGroup.subGroup.forEach(item=>{
+                        item.status=true;
+                    })
+                }else{
+                    primaryGroup.subGroup.forEach(item=>{
+                        item.status=false;
+                    })
+                }
+            })
+        } 
+    },
 }
 </script>
 
 <style>
-.demo-table-expand {
-    font-size: 0;
+.thead{
+    border-bottom: 1px solid #eee;
+    padding: 10px 20px;
 }
-.demo-table-expand label {
-    width: 90px;    
-    color: #99a9bf;
+.thead span{
+    font-weight: 600;
 }
-.demo-table-expand .el-form-item {
-    margin-right: 0;
-    margin-bottom: 0;
-    width: 50%;
+.caoz-a{
+    text-align: right;
+    width: 100%;
 }
-.zhed-btn{
-    border: 1px solid rgb(83, 163, 255);
-    color:rgb(83, 163, 255);
+.caoz-a a{
+    color:cornflowerblue;
+    margin-right: 20px;
+    margin-left: 0px;
+}
+
+.tbody{
+    margin-left: 100px;
+}
+.tbody .el-row{
+    border-bottom: 1px solid #eee;
+    padding-right: 20px;
+    height: 50px;
+}
+.tbody .el-row .el-col{
+    display: flex;
+}
+.tbody .el-row .el-col div{
+    align-self: center;
+}
+.tbody img{
+    width: 40px;
+}
+.imgTitle *{
+    vertical-align:middle
 }
 </style>
